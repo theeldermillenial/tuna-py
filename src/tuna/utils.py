abb809569e57d161407f671faa5a954bee77d39c
Elder Millenial <eldermillenial@protonmail.com>
2024-08-30 22:20:37 -0400

    Removed blockfrost

--- src/tuna/utils.py
+++ src/tuna/utils.py
@@ -1,33 +1,4 @@
-import os
 from hashlib import sha256
-
-import blockfrost
-from dotenv import load_dotenv
-from pycardano import DeserializeException
-from tuna.datums import StateV2
-from tuna.config import CONFIG
-
-load_dotenv()
-
-client = blockfrost.BlockFrostApi(
-    project_id=os.environ["PROJECT_ID"], base_url=blockfrost.ApiUrls.preview.value
-)
-
-
-def latest_block() -> StateV2:
-
-    utxos = client.address_utxos(
-        address=CONFIG["preview"]["address"], return_type="json"
-    )
-
-    for utxo in utxos:
-        if utxo["inline_datum"] is not None:
-            try:
-                return StateV2.from_cbor(utxo["inline_datum"])
-            except DeserializeException:
-                continue
-
-    raise ValueError("No utxo found.")
 
 
 def get_hash(payload: bytes):
